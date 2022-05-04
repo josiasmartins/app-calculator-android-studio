7ea3a7bc7116373a86b842e10a0f604d3976215b
Caitano <josiasmartinscaitano@gmail.com>
2022-05-04 18:47:46 -0300

    fixture: adicionado a função de somar

--- app/src/main/java/josias/m/calculadora/MainActivity.java
+++ app/src/main/java/josias/m/calculadora/MainActivity.java
@@ -5,10 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtNro1;
+    private EditText txtNro2;
+    private TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtNro1 = findViewById(R.id.txtNro1);
+        txtNro2 = findViewById(R.id.txtNro2);
+        tvResultado = findViewById(R.id.tvResultado);
+    }
+
+    public void somar(View view) {
+        int valor1 = Integer.parseInt(txtNro1.getText().toString());
+        int valor2 = Integer.parseInt(txtNro2.getText().toString());
+
+        tvResultado.setText(String.valueOf(valor1 + valor2));
     }
 }
